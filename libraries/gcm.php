d57ce0a2837daf552f6260a2d2f222785f447933
Eduardo K <eduard@kozachek.net>
2014-12-12 21:13:40 +0200

    Change license from GPL to BSD

--- libraries/gcm.php
+++ libraries/gcm.php
@@ -5,7 +5,16 @@
  * @copyright (c) 2012 AntonGorodezkiy
  * info: https://github.com/antongorodezkiy/codeigniter-gcm/
  * Description: PHP Codeigniter Google Cloud Messaging Library
- * License: GNU/GPL 2
+ * License: BSD
+ * 
+ * Copyright (c) 2012, AntonGorodezkiy
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
  */
 
 class GCM {
@@ -151,7 +160,7 @@ class GCM {
 		$this->payload['registration_ids'] = array_unique($this->payload['registration_ids']);
 		
 		if (isset($this->payload['time_to_live']) && !isset($this->payload['collapse_key']))
-			$this->payload['collapse_key'] = 'Punchmo Notifications';
+			$this->payload['collapse_key'] = 'GCM Notifications';
 		
 		$data = json_encode($this->payload);
 		return $this->request($data);
